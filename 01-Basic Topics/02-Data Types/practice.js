35482ee46b18fe81f9610e1f72a7cd59312a6d4d
mesomnath <somnathsahu.dev@gmail.com>
2025-12-09 14:19:57 +0530

    data type - practice

--- 01-Basic Topics/02-Data Types/practice.js
+++ 01-Basic Topics/02-Data Types/practice.js
@@ -9,3 +9,15 @@ console.log(typeof {});          // "object"
 console.log(typeof []);          // "object"
 console.log(typeof function(){}); // "function"
 console.log(typeof {"json":"dfd"})
+console.log(typeof alert);
+
+
+/*
+ === means it will verify both the values along with data type. if value along with data type is mismatched It will return false.
+*/
+
+let a = 3;
+let b = "3";
+
+console.log(a==b); //true
+console.log(a===b); // false
