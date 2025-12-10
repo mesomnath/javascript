e8a6a0127f8c88c51faa1eb62d064d1ee98aa500
mesomnath <somnathsahu.dev@gmail.com>
2025-12-10 10:47:09 +0530

    data type lecture practice

--- 01-Basic Topics/02-Data Types/practice.js
+++ 01-Basic Topics/02-Data Types/practice.js
@@ -7,9 +7,9 @@ console.log(typeof Symbol("id")); // "symbol"
 console.log(typeof 123n);        // "bigint"
 console.log(typeof {});          // "object"
 console.log(typeof []);          // "object"
-console.log(typeof function(){}); // "function"
-console.log(typeof {"json":"dfd"})
-console.log(typeof alert);
+console.log(typeof function(){}); // "function"/ "function object"
+console.log(typeof {"json":"dfd"}) //object
+console.log(typeof alert);  // undefined
 
 
 /*
@@ -21,3 +21,21 @@ let b = "3";
 
 console.log(a==b); //true
 console.log(a===b); // false
+
+//--- Primitive data type -----//
+
+// there are total 7 data types: string, number, boolean, undefined, null, bigint, Symbol
+
+//----- Reference (Non-primitive data type)
+// array, objects, functions
+//*** All non primitive data type has a Object data type.  */
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj = {
+    name: "somnath",
+    age: 22
+}
+// arrays started with [], and objects started with {}
+
+const myFunction = function(){
+    console.log("hello world");
+}
